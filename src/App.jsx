195740c08f2ffddf7d2d67a07667509ddd60efba
PabloLUC9832 <lucaspablo340@outlook.com>
2023-09-04 12:32:04 -0600

    creacion con Vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div className="bg-gradient-to-r from-blue-500">
+        Hola mundo
+      </div>
+  )
+}
+
+export default App
