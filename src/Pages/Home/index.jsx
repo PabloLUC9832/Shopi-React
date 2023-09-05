ad85360c5b8c98bd6132fa79ff02b7dee8f38a90
PabloLUC9832 <lucaspablo340@outlook.com>
2023-09-05 11:18:48 -0600

    consumo de fakeAPI

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,27 @@
 import {Layout} from "../../Components/Layout/index.jsx";
 import {Card} from "../../Components/Card/index.jsx";
+import {useEffect, useState} from "react";
 
 function Home() {
+
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+            .then(response => response.json())
+            .then(data => setItems(data))
+    }, []);
+
     return (
         <Layout>
             Home
-            <Card />
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen">
+                {
+                    items ?.map(item => (
+                        <Card data={item} key={item.id}/>
+                    ))
+                }
+            </div>
         </Layout>
     );
 }
