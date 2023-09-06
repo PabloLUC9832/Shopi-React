4158a779f7e71ee697f450712f3565f1ef341639
PabloLUC9832 <lucaspablo340@outlook.com>
2023-09-06 17:00:32 -0600

    18-bloquear el icono de add, si ya se añadio

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import {ShoppingCartContext} from "../../Contex/index.jsx";
-import { PlusIcon } from '@heroicons/react/24/solid'
+import {CheckIcon, PlusIcon} from '@heroicons/react/24/solid'
 
 const Card = (data) => {
 
@@ -18,6 +18,31 @@ const Card = (data) => {
         context.closeProductDetail();
     }
 
+    const renderIcon = (id) => {
+
+        const isInCart = context.cartProducts.filter(product => product.id === id).length > 0;
+
+        if(isInCart){
+            return (
+                <div
+                    className="absolute top-0 right-0 flex justify-center items-center bg-black w-6 h-6 rounded-full m-2 p-1 cursor-default">
+                    <CheckIcon className="h-6 w-6 text-white"></CheckIcon>
+                </div>
+            );
+        }else{
+            return (
+                <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
+                     onClick={
+                         (event) => addProductToCard(event, data.data)
+                     }
+                >
+                    <PlusIcon className="h-6 w-6 text-black"></PlusIcon>
+                </div>
+            );
+        }
+
+    }
+
     return (
         <div className="bg-white cursor-pointer w-56 h-60 rounded-lg"
              onClick={() => showProduct(data.data)}>
@@ -26,13 +51,7 @@ const Card = (data) => {
                 >{data.data.category.name}</span>
                 <img className="w-full h-full object-cover rounded-lg "
                     src={data.data.images?.[0]}  alt={data.data.title}/>
-                <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-                onClick={
-                    (event) => addProductToCard(event, data.data)
-                }
-                >
-                    <PlusIcon className="h-6 w-6 text-black"></PlusIcon>
-                </div>
+                { renderIcon(data.data.id) }
             </figure>
             <p className="flex justify-between">
                 <span className="text-sm font-light">{data.data.title}</span>
