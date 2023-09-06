e380608333c38d5f80ef7b9c628b1a61c1cb5e3f
PabloLUC9832 <lucaspablo340@outlook.com>
2023-09-05 19:48:17 -0600

    14-datos en el product detail

--- src/Contex/index.jsx
+++ src/Contex/index.jsx
@@ -4,16 +4,23 @@ const ShoppingCartContext = createContext();
 
 const ShoppingCartContextProvider = ({children}) => {
 
+    //Shopping Cart - Increment quantity
     const [count, setCount] = useState(0);
+
+    //Product detail - Open Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
     const openProductDetail = () => setIsProductDetailOpen(true);
     const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    //Product Detail - Show Product
+    const [productToShow, setProductToShow] = useState({});
 
     return (
         <ShoppingCartContext.Provider value={{
             count,setCount,
             openProductDetail,closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,productToShow,
+            setProductToShow,
         }}>
             {children}
         </ShoppingCartContext.Provider>
